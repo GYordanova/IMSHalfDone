38eb2d388186291bcd55325afed9a60202737824
Piers Barber <Kochab.96@gmail.com>
2020-11-19 14:46:24 +0000

    For Trainees

---
+++ src/main/java/com/qa/ims/utils/Utils.java
@@ -0,0 +1,65 @@
+package com.qa.ims.utils;
+
+import java.util.Scanner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Utils {
+	private static Logger LOGGER = LogManager.getLogger();
+
+	private final Scanner scanner;
+
+	public Utils(Scanner scanner) {
+		super();
+		this.scanner = scanner;
+	}
+
+	public Utils() {
+		scanner = new Scanner(System.in);
+	}
+
+	public Long getLong() {
+		String input = getString();
+		Long longInput = null;
+		do {
+			try {
+				longInput = Long.parseLong(input);
+			} catch (NumberFormatException nfe) {
+				LOGGER.info("Error - Please enter a number");
+			}
+		} while (longInput == null);
+		return longInput;
+	}
+
+	public String getString() {
+		return scanner.nextLine();
+	}
+	
+	public int getInt() {
+		int returnint;
+		while(true) {
+			try {
+				returnint = scanner.nextInt();
+				break;
+			} catch (Exception e) {
+				LOGGER.info("Error - Please enter an int");
+			}
+		}
+		return returnint;
+	}
+
+	public Double getDouble() {
+		String input = getString();
+		Double doubleInput = null;
+		do {
+			try {
+				doubleInput = Double.parseDouble(input);
+			} catch (NumberFormatException nfe) {
+				LOGGER.info("Error - Please enter a number");
+			}
+		} while (doubleInput == null);
+		return doubleInput;
+	}
+
+}
